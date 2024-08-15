e773c1f4a283965212d7264eca6677189ad52718
Ilia <43654815+istudyatuni@users.noreply.github.com>
2024-08-15 10:04:42 +0300

    restore terminal on error (#51)
    * restore terminal on error
    
    * Update src/main.rs
    
    Co-authored-by: Ilia <43654815+istudyatuni@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Cretezy <Cretezy@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -68,24 +68,12 @@ fn main() -> Result<()> {
     // Setup app
     let mut app = App::new(env.clone(), &mut commander)?;
 
-    // Setup terminal
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    let mut terminal = setup_terminal()?;
 
     // Run app
-    run_app(&mut terminal, &mut app, &mut commander)?;
-
-    // Restore terminal
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
+    let res = run_app(&mut terminal, &mut app, &mut commander);
+    restore_terminal(terminal)?;
+    res?;
 
     Ok(())
 }
@@ -109,6 +97,25 @@ fn run_app<B: Backend>(
             return Ok(());
         }
     }
+}
+
+fn setup_terminal() -> Result<Terminal<CrosstermBackend<io::Stdout>>> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    Ok(Terminal::new(backend)?)
+}
+
+fn restore_terminal(mut terminal: Terminal<CrosstermBackend<io::Stdout>>) -> Result<()> {
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+    Ok(())
 }
 
 enum ComponentInputResult {
