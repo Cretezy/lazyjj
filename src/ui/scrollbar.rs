0ff44f5d8cca2f5b89fadd31b37af51d9be4f899
Ilia <istudyatuni@gmail.com>
2025-05-30 21:15:03 +0300

    chore: refactor scrollbar a bit

--- src/ui/scrollbar.rs
+++ src/ui/scrollbar.rs
@@ -2,10 +2,7 @@ use ratatui::{
     buffer::Buffer,
     layout::{Margin, Rect},
     style::Style,
-    symbols::{
-        block::FULL,
-        line::{DOUBLE_HORIZONTAL, DOUBLE_VERTICAL},
-    },
+    symbols::scrollbar::{HORIZONTAL, VERTICAL},
     widgets::Widget,
     Frame,
 };
@@ -21,8 +18,6 @@ pub enum Orientation {
 struct Scrollbar {
     max: u16,
     pos: u16,
-    style_bar: Style,
-    style_pos: Style,
     orientation: Orientation,
 }
 
@@ -31,89 +26,72 @@ impl Scrollbar {
         Self {
             max: u16::try_from(max).unwrap_or_default(),
             pos: u16::try_from(pos).unwrap_or_default(),
-            style_pos: Style::default(),
-            style_bar: Style::default(),
             orientation,
         }
     }
 
+    fn calc_pos(&self, scrollbar_lenght: u16) -> u16 {
+        let progress = f32::from(self.pos) / f32::from(self.max);
+        let progress = progress.min(1.0);
+        let pos = f32::from(scrollbar_lenght) * progress;
+
+        let pos: u16 = (pos + 0.5) as u16;
+        pos.saturating_sub(1)
+    }
+
     fn render_vertical(self, area: Rect, buf: &mut Buffer) {
-        if area.height <= 2 {
-            return;
-        }
-
-        if self.max == 0 {
-            return;
-        }
-
-        let right = area.right().saturating_sub(1);
-        if right <= area.left() {
+        let area_right = area.right().saturating_sub(1);
+        if area.height <= 2 || area_right <= area.left() {
             return;
         }
 
         let (bar_top, bar_height) = {
-            let scrollbar_area = area.inner(Margin {
-                horizontal: 0,
-                vertical: 1,
-            });
-
+            let scrollbar_area = area.inner(Margin::new(0, 1));
             (scrollbar_area.top(), scrollbar_area.height)
         };
 
+        let style = Style::default();
         for y in bar_top..(bar_top + bar_height) {
-            buf.set_string(right, y, DOUBLE_VERTICAL, self.style_bar);
+            buf.set_string(area_right, y, VERTICAL.track, style);
         }
-
-        let progress = f32::from(self.pos) / f32::from(self.max);
-        let progress = if progress > 1.0 { 1.0 } else { progress };
-        let pos = f32::from(bar_height) * progress;
-
-        let pos: u16 = (pos + 0.5) as u16;
-        let pos = pos.saturating_sub(1);
-
-        buf.set_string(right, bar_top + pos, FULL, self.style_pos);
+        buf.set_string(
+            area_right,
+            bar_top + self.calc_pos(bar_height),
+            VERTICAL.thumb,
+            style,
+        );
     }
 
     fn render_horizontal(self, area: Rect, buf: &mut Buffer) {
-        if area.width <= 2 {
-            return;
-        }
-
-        if self.max == 0 {
-            return;
-        }
-
-        let bottom = area.bottom().saturating_sub(1);
-        if bottom <= area.top() {
+        let area_bottom = area.bottom().saturating_sub(1);
+        if area.width <= 2 || area_bottom <= area.top() {
             return;
         }
 
         let (bar_left, bar_width) = {
-            let scrollbar_area = area.inner(Margin {
-                horizontal: 1,
-                vertical: 0,
-            });
-
+            let scrollbar_area = area.inner(Margin::new(1, 0));
             (scrollbar_area.left(), scrollbar_area.width)
         };
 
+        let style = Style::default();
         for x in bar_left..(bar_left + bar_width) {
-            buf.set_string(x, bottom, DOUBLE_HORIZONTAL, self.style_bar);
+            buf.set_string(x, area_bottom, HORIZONTAL.track, style);
         }
-
-        let progress = f32::from(self.pos) / f32::from(self.max);
-        let progress = if progress > 1.0 { 1.0 } else { progress };
-        let pos = f32::from(bar_width) * progress;
-
-        let pos: u16 = (pos + 0.5) as u16;
-        let pos = pos.saturating_sub(1);
-
-        buf.set_string(bar_left + pos, bottom, FULL, self.style_pos);
+        buf.set_string(
+            bar_left + self.calc_pos(bar_width),
+            area_bottom,
+            HORIZONTAL.thumb,
+            style,
+        );
     }
 }
 
 impl Widget for Scrollbar {
     fn render(self, area: Rect, buf: &mut Buffer) {
+        if self.max == 0 {
+            return;
+        }
+
         match &self.orientation {
             Orientation::Vertical => self.render_vertical(area, buf),
             Orientation::Horizontal => {
